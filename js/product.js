8c526c725def3941d8e4e32b1ce944604a9002d6
skwizo <bayimostapha@gmail.com>
2023-10-07 12:05:20 +0100

    enhanced quantity input functionnality

--- js/product.js
+++ js/product.js
@@ -1,27 +1,37 @@
 const colorWrappers = document.querySelectorAll(".product-color-wrapper");
-colorWrappers.forEach(colorWrapper => {
-    const radio = colorWrapper.querySelector(".color-radio");
-    const label = colorWrapper.querySelector("label");
+if (colorWrappers)
+    colorWrappers.forEach(colorWrapper => {
+        const radio = colorWrapper.querySelector(".color-radio");
+        const label = colorWrapper.querySelector("label");
 
-    radio.addEventListener("click", () => {
-        colorWrappers.forEach(otherColorWrapper => {
-            otherColorWrapper.querySelector("label").classList.remove("checked");
+        radio.addEventListener("click", () => {
+            colorWrappers.forEach(otherColorWrapper => {
+                otherColorWrapper.querySelector("label").classList.remove("checked");
+            });
+            label.classList.add("checked");
         });
-        label.classList.add("checked");
     });
-});
 
 const quantityInput = document.querySelector(".quantity");
 const plusBtn = document.querySelector(".plus");
 const minusBtn = document.querySelector(".minus");
-plusBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let quantity = parseInt(quantityInput.value);
-    quantityInput.value = quantity + 1;
-});
-minusBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let quantity = parseInt(quantityInput.value);
-    if (quantity >= 2)
-        quantityInput.value = quantity - 1;
-});
+if (plusBtn && quantityInput)
+    plusBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        let quantity = parseInt(quantityInput.value);
+        quantityInput.value = quantity + 1;
+    });
+if (minusBtn && quantityInput)
+    minusBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        let quantity = parseInt(quantityInput.value);
+        if (quantity >= 2)
+            quantityInput.value = quantity - 1;
+    });
+if (quantityInput)
+    quantityInput.addEventListener('input', () => {
+        let n = quantityInput.value.replace(/[^0-9]/g, '');
+        if (n == '')
+            n = 1;
+        quantityInput.value = n;
+    });
